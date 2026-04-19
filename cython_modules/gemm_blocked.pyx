a002637df2bbdd6e56f0c4003dfe100d0faf2050
Pablo Gonzalez Segarra <pablogscraft@gmail.com>
2026-04-19 18:29:12 +0200

    checko jota

--- cython_modules/gemm_blocked.pyx
+++ cython_modules/gemm_blocked.pyx
@@ -3,6 +3,8 @@
 import numpy as np
 cimport numpy as cnp
 
+# Inicio bloque asistido por la IA
+
 def gemm_blocked(
     cnp.ndarray[cnp.float32_t, ndim=2] A,
     cnp.ndarray[cnp.float32_t, ndim=2] B,
@@ -14,8 +16,9 @@ def gemm_blocked(
     cdef int N = B.shape[1]
     cdef int ic, jc, pc
 
-    # Inicio bloque asistido por la IA
 
+    # Recorremos las matrices en bloques, primero por columnas de C/B (NC), luego por la parte común de A/B (KC), y finalmente por filas de C/A (MC)
+    # Multiplacndo des esta forma, conseguimos que el bloque de B se mantenga en caché mientras se multiplica con diferentes bloques de A maximizando la reutilización de caché.
     for jc in range(0, N, NC):
         for pc in range(0, K, KC):
             for ic in range(0, M, MC):
@@ -24,5 +27,6 @@ def gemm_blocked(
                     B[pc:pc+KC, jc:jc+NC]
                 )
 
-    # Fin bloque asistido
     return C
+
+# Fin bloque asistido por la IA
